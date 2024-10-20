f72b2275fb96c6e9d4beacbffe301a4810a66e84
Dima Levchenko <112283201+levandowskiy@users.noreply.github.com>
2024-10-21 02:55:46 +0300

    fix b

--- src/api/helpers/instance.js
+++ src/api/helpers/instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'https://twitter-backend-xp2w.onrender.com/',
+  baseURL: 'https://twitter-backend-xp2w.onrender.com',
   headers: {
     Accept: 'application/json',
     'Content-type': 'application/json',
