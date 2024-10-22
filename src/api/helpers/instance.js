66caf973efb4f5540968caf80908382bd1b6eb71
Dima Levchenko <112283201+levandowskiy@users.noreply.github.com>
2024-10-22 22:20:04 +0300

    add baseURL for axios

--- src/api/helpers/instance.js
+++ src/api/helpers/instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'https://twitter-backend-xp2w.onrender.com/api',
+  baseURL: 'https://twitter-backend-32hl.onrender.com/api',
   headers: {
     Accept: 'application/json',
     'Content-type': 'application/json',
